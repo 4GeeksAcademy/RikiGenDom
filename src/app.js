f6f6bcccacc10e03292e7857abcb6f528cb7db56
RikiTeethdog <flanseros@yahoo.es>
2025-01-30 11:30:38 +0000

    Hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,18 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+  let pronoun = ['the', 'her', 'this'];
+  let adj = ['amazing', 'tiny', 'beautiful'];
+  let noun = ['motherfucker', 'revolver', 'machine'];
+  let terms = ['.com.uk', '.com', '.agr', '.io']
+
+pronoun.forEach((kiwi) => {
+  adj.forEach((slack) => {
+    noun.forEach((javi) => {
+      terms.forEach((miguel) => {
+        console.log(kiwi + slack + javi + miguel)
+      })
+    })
+  })
+})
